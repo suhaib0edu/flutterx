8ab7ac751be699ee3a821f1b2ddfe54efd1abcd5
Suhaib Eltayeb <suhab.edu@gmail.com>
2022-09-16 04:25:59 -0700

    app bar is done

--- lib/material/app_bar.dart
+++ lib/material/app_bar.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class AppBarDemo extends StatelessWidget {
@@ -6,6 +5,26 @@ class AppBarDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+     appBar: AppBar(
+       leading: Builder(builder: (context){
+         return IconButton(tooltip: 'menu', onPressed: (){}, icon: const Icon(Icons.menu));
+       }),
+       title: const Text('flutter'),
+       actions: [
+         IconButton(tooltip: 'telegram',onPressed: (){}, icon: const Icon(Icons.telegram)),
+         IconButton(tooltip: 'facebook',onPressed: (){}, icon: const Icon(Icons.facebook)),
+         IconButton(tooltip: 'youtube_searched_for',onPressed: (){}, icon: const Icon(Icons.youtube_searched_for)),
+         PopupMenuButton(itemBuilder: (cont){
+           return const [
+             PopupMenuItem(child: Text('data1')),
+             PopupMenuItem(child: Text('data2')),
+             PopupMenuItem(child: Text('data3')),
+             PopupMenuItem(child: Text('data4')),
+           ];
+         })
+       ],
+     ),
+    );
   }
 }
