84d194355a64943271bccf746ee13f5ede8cb139
Suhaib Eltayeb <suhab.edu@gmail.com>
2022-09-16 04:26:56 -0700

    app bar is done

--- lib/material/app_bar.dart
+++ lib/material/app_bar.dart
@@ -6,25 +6,35 @@ class AppBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-       leading: Builder(builder: (context){
-         return IconButton(tooltip: 'menu', onPressed: (){}, icon: const Icon(Icons.menu));
-       }),
-       title: const Text('flutter'),
-       actions: [
-         IconButton(tooltip: 'telegram',onPressed: (){}, icon: const Icon(Icons.telegram)),
-         IconButton(tooltip: 'facebook',onPressed: (){}, icon: const Icon(Icons.facebook)),
-         IconButton(tooltip: 'youtube_searched_for',onPressed: (){}, icon: const Icon(Icons.youtube_searched_for)),
-         PopupMenuButton(itemBuilder: (cont){
-           return const [
-             PopupMenuItem(child: Text('data1')),
-             PopupMenuItem(child: Text('data2')),
-             PopupMenuItem(child: Text('data3')),
-             PopupMenuItem(child: Text('data4')),
-           ];
-         })
-       ],
-     ),
+      appBar: AppBar(
+        leading: Builder(builder: (context) {
+          return IconButton(
+              tooltip: 'menu', onPressed: () {}, icon: const Icon(Icons.menu));
+        }),
+        title: const Text('flutter'),
+        actions: [
+          IconButton(
+              tooltip: 'telegram',
+              onPressed: () {},
+              icon: const Icon(Icons.telegram)),
+          IconButton(
+              tooltip: 'facebook',
+              onPressed: () {},
+              icon: const Icon(Icons.facebook)),
+          IconButton(
+              tooltip: 'youtube_searched_for',
+              onPressed: () {},
+              icon: const Icon(Icons.youtube_searched_for)),
+          PopupMenuButton(itemBuilder: (cont) {
+            return const [
+              PopupMenuItem(child: Text('data1')),
+              PopupMenuItem(child: Text('data2')),
+              PopupMenuItem(child: Text('data3')),
+              PopupMenuItem(child: Text('data4')),
+            ];
+          })
+        ],
+      ),
     );
   }
 }
