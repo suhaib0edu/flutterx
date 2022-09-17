924e8390a1f7acccc0e0dd8ffe4bd797a4aea352
Suhaib Eltayeb <suhab.edu@gmail.com>
2022-09-17 00:44:45 -0700

    Add banner

--- lib/material/app_bar.dart
+++ lib/material/app_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'banner.dart';
+
 class AppBarDemo extends StatelessWidget {
   const AppBarDemo({Key? key}) : super(key: key);
 
@@ -7,25 +9,15 @@ class AppBarDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Builder(builder: (context) {
-          return IconButton(
-              tooltip: 'menu', onPressed: () {}, icon: const Icon(Icons.menu));
+        leading: Builder(builder: (context){
+          return IconButton(tooltip: 'menu', onPressed: (){}, icon: const Icon(Icons.menu));
         }),
         title: const Text('flutter'),
         actions: [
-          IconButton(
-              tooltip: 'telegram',
-              onPressed: () {},
-              icon: const Icon(Icons.telegram)),
-          IconButton(
-              tooltip: 'facebook',
-              onPressed: () {},
-              icon: const Icon(Icons.facebook)),
-          IconButton(
-              tooltip: 'youtube_searched_for',
-              onPressed: () {},
-              icon: const Icon(Icons.youtube_searched_for)),
-          PopupMenuButton(itemBuilder: (cont) {
+          IconButton(tooltip: 'telegram',onPressed: (){}, icon: const Icon(Icons.telegram)),
+          IconButton(tooltip: 'facebook',onPressed: (){}, icon: const Icon(Icons.facebook)),
+          IconButton(tooltip: 'youtube_searched_for',onPressed: (){}, icon: const Icon(Icons.youtube_searched_for)),
+          PopupMenuButton(itemBuilder: (cont){
             return const [
               PopupMenuItem(child: Text('data1')),
               PopupMenuItem(child: Text('data2')),
@@ -35,6 +27,7 @@ class AppBarDemo extends StatelessWidget {
           })
         ],
       ),
+     // body: const BannerDemo(),
     );
   }
 }
